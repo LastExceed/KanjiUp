a0a2555eaf458111f61b0113e2e8ca8491c017f9
Malox <Maxi.Junges@gmx.de>
2022-09-23 13:59:03 +0200

    Fix version bump

--- app/src/main/java/com/github/lastexceed/kanjiup/MainActivity.kt
+++ app/src/main/java/com/github/lastexceed/kanjiup/MainActivity.kt
@@ -1,22 +1,9 @@
 package com.github.lastexceed.kanjiup
 
-import android.os.*
-import androidx.activity.*
-import androidx.activity.compose.*
-import androidx.compose.animation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.*
-import androidx.compose.material.icons.rounded.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.*
-import androidx.compose.ui.draw.*
-import androidx.compose.ui.graphics.*
-import androidx.compose.ui.text.style.*
-import androidx.compose.ui.tooling.preview.*
-import androidx.compose.ui.unit.*
-import com.github.lastexceed.kanjiup.ui.theme.*
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.github.lastexceed.kanjiup.ui.theme.KanjiUpTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
