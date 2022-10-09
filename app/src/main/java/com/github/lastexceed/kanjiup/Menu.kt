99749dcb7699c44107394469cb1b38a32ad6ccf6
LastExceed <chris20194@googlemail.com>
2022-10-09 13:12:36 +0200

    remove empty scaffold

--- app/src/main/java/com/github/lastexceed/kanjiup/Menu.kt
+++ app/src/main/java/com/github/lastexceed/kanjiup/Menu.kt
@@ -2,56 +2,22 @@ package com.github.lastexceed.kanjiup
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.*
 
 @Composable
 fun Menu(navController: NavController) {
-	Scaffold(
-		//topBar = {
-		//	TopAppBar {
-		//		IconButton(onClick = {
-		//			if (navController.currentBackStackEntry != null) {
-		//				navController.navigateUp()
-		//			}
-		//		}) {
-		//			Icon(
-		//				imageVector = Icons.Rounded.ArrowBack,
-		//				contentDescription = "arrow back",
-		//				modifier = Modifier.align(alignment = Alignment.CenterVertically)
-		//			)
-		//		}
-		//	}
-		//}
-	) { contentPadding ->
-		MenuButtons(Modifier.padding(contentPadding)) { destination: String ->
-			navController.navigate(destination)
-		}
-	}
-}
-
-
-@Composable
-fun MenuButtons(modifier: Modifier, onClick: (String) -> Unit) {
 	Column(
-		modifier
+		Modifier
 			.fillMaxSize()
 			.padding(50.dp),
 		verticalArrangement = Arrangement.spacedBy(20.dp)
 	) {
 		RectangleButton(
-			onClick = { onClick("vocab_learning") },
+			onClick = { navController.navigate("vocab_learning") },
 			modifier = Modifier.size(width = 300.dp, height = 50.dp),
 			backgroundColor = MaterialTheme.colors.primary
 		) {
@@ -59,7 +25,7 @@ fun MenuButtons(modifier: Modifier, onClick: (String) -> Unit) {
 		}
 
 		RectangleButton(
-			onClick = { onClick("settings_page") },
+			onClick = { navController.navigate("settings_page") },
 			modifier = Modifier.size(width = 300.dp, height = 50.dp),
 			backgroundColor = MaterialTheme.colors.primary
 		) {
@@ -68,8 +34,8 @@ fun MenuButtons(modifier: Modifier, onClick: (String) -> Unit) {
 	}
 }
 
-@Preview
-@Composable
-fun MenuButtonsPreview() {
-	MenuButtons(onClick = { }, modifier = Modifier)
-}
+//@Preview
+//@Composable
+//fun MenuButtonsPreview() {
+//	Menu(onButtonClicked = { })
+//}
